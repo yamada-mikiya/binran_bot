ab5508798140c72689f7e9cf383732ac87cec936
山田 光樹矢 <167268849+yamada-mikiya@users.noreply.github.com>
2025-06-08 14:23:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def get_conversational_chain():
 
     【回答】
     """
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "chat_history", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
